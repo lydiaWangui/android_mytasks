42e1e6100f3bfc0731f306326743b7e649895feb
lydiaWangui <lydiawanguid@gmail.com>
2019-07-04 21:05:28 +0300

    initial commit

---
+++ app/src/main/java/Adapter.java
@@ -0,0 +1,53 @@
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lydia.mytasks.R;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>  {
+    private List<ListItem> listItems;
+    private Context context;
+
+    public Adapter(List<ListItem> listItems, Context context) {
+        this.listItems = listItems;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list,viewGroup,false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        ListItem listItem = listItems.get(i);
+        viewHolder.textViewHead.setText(listItem.getHead());
+        viewHolder.textViewDesc.setText(listItem.getDesc());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView textViewHead;
+        public TextView textViewDesc;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewDesc = (TextView) itemView.findViewById(R.id.textViewDesc);
+            textViewHead = (TextView) itemView.findViewById(R.id.textViewHead);
+
+        }
+    }
+}
